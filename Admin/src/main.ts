325ae2809f8e2fc992a5a24f8622743fea304478
Brett Meyer <bmeyer0127@gmail.com>
2024-11-26 20:27:42 -0500

    Choosing between vuetify and vite base setup

--- Admin/src/main.ts
+++ Admin/src/main.ts
@@ -1,11 +1,12 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import vuetify from './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+app.use(vuetify)
 app.use(router)
 
 app.mount('#app')
